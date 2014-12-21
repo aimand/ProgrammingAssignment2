c764cb7903bc38351f6af74405b3961f8dbcad3b
aimand <aimand14@gmail.com>
2014-12-21 14:41:47 -0500

    Implemented Functions
    The two functions: makeCacheMatrix and cacheSolve were implemented.

--- cachematrix.R
+++ cachematrix.R
@@ -9,6 +9,20 @@
 ## 	This function gets the Inversed Matrix
 ## The usage of the assignment operator '<<-' will prevent the variables
 ## from being exposed to the outside world.
+##
+##************************* How to test ***********************
+## 1. Change the diretory in RStudio to the folder where cacheMatrix.R resides
+## 2. Load the cacheMatrix.R:
+## 	> source("/cachematrix.R")
+## 3. Create a test matrix:
+## 	> mTest <- matrix(runif(16,1,20),4,4)
+## 4. Generate the makeCacheMatrix object:
+##	> cTest <- makeCacheMatrix(mTest)
+## 5. Solve for the inverse of the matrix:
+##	> invTest <- cacheSolve(cTest)
+## 6. If desired display inversed matrix
+##	> invTest
+
 
 
 makeCacheMatrix <- function(x = matrix()) {
